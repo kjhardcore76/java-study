3fcc42b675f74218dfbfef300ff2d91c9ce0407a
DESKTOP-HI2REUQ\G203 <kjhardcore19760602@gmail.com>
2024-06-25 16:53:54 +0900

    자바 논리 연산자

---
+++ Project-03/src/Quiz/Quiz6.java
@@ -0,0 +1,10 @@
+package Quiz;
+
+public class Quiz6 {
+	public static void main(String[] args) {
+		String result1 = (7%2) == 1?"7은홀수":"7은짝수";
+		String result2 = (10%2) == 1?"10은홀수":"10은짝수";
+		System.out.println(result1+"  "+result2);
+		
+	}
+}
---
+++ Project-03/src/Quiz/Quiz7.java
@@ -0,0 +1,18 @@
+package Quiz;
+
+public class Quiz7 {
+
+	public static void main(String[] args) {
+		boolean result1 = (1 == 2) || (1 < 2);// true (1<2를 만족함)
+		boolean result2 = true && false; // false (false return after true)
+		boolean result3 = !false; // navgative true 
+		int x = 1;
+		int y = 5;
+		boolean result4 = (x < y) || (x == y);// true (x<y를 만족함)
+	}
+
+}
+
+
+
+
