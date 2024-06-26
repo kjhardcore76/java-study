6dd925d46cbdeadf9b5988573064d29e5e66e1b8
DESKTOP-HI2REUQ\G203 <kjhardcore19760602@gmail.com>
2024-06-26 16:50:11 +0900

    조건문

---
+++ Project4/src/condition/Ex1.java
@@ -0,0 +1,19 @@
+package condition;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 10;
+		if(age >= 8) {
+			System.out.println("학교에 다닙니다");
+		}
+		
+		if(age >= 8) {
+			int temp = 0;
+			System.out.println(temp);
+		}
+		//System.out.println(temp);
+	} 
+
+}
