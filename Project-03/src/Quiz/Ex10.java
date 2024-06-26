868d799dfeb127e34cec6c901aa72a9c8664eec7
DESKTOP-HI2REUQ\G203 <kjhardcore19760602@gmail.com>
2024-06-26 15:29:35 +0900

    삼항연산자까지~~

---
+++ Project-03/src/Quiz/Ex10.java
@@ -0,0 +1,14 @@
+package Quiz;
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int result1 = -5 + 3 * 10 / 2; //10
+		System.out.println(result1);
+		int x = 1;
+		int result2 = (++x) + 5;//7
+		System.out.println(result2);
+	}
+
+}
