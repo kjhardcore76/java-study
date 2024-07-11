1d07e5fe5456527b808fe1110b66cf8e64a49966
DESKTOP-HI2REUQ\G203 <kjhardcore19760602@gmail.com>
2024-07-11 18:51:33 +0900

    list 수업중

--- 07/src/array_basic/quiz/Quiz2.java
+++ 07/src/array_basic/quiz/Quiz2.java
@@ -1,19 +1,37 @@
 package array_basic.quiz;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Quiz2 {
 
 	public static void main(String[] args) {
-		int[] intArr = {1,2,3,4,5};
-		int leng = intArr.length;
-		int[] sortArr = new int[leng];
-		
-		for(int i=0; i < leng ; i ++) {
-			
-		}
-		
-		System.arraycopy(intArr, 0, sortArr, 0, leng);
-	
+		Student student1 = new Student("학생1", 10);
+		Student student2 = new Student("학생2", 10);
+		Student student3 = new Student("학생3", 10);
+		Student student4 = new Student("학생4", 10);
+		List<Student> studentList = new ArrayList<Student>();
+		studentList.add(student1);
+		studentList.add(student2);
+		studentList.add(student3);
+		studentList.add(student4);
+		studentList.forEach((student) -> {
+			String studentInfo = student.getInfo();
+			System.out.println(studentInfo);
+		});
 	}
 
+}
+
+class Student {
+	String name;
+	int age;
+	public Student(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	public String getInfo() {
+		return this.name+" "+this.age;
+	}
 }
